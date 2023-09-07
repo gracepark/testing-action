37010658b2af73bb15aab6599f6f32f89a493c96
Grace Park <gracepark@github.com>
2023-09-07 12:14:23 -0700

    yes

--- src/content-linter/scripts/markdownlint.js
+++ src/content-linter/scripts/markdownlint.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { program, Option } from 'commander'
+import { program } from 'commander'
+import { extname } from 'path'
 
 program
   .description('Run GitHub Docs Markdownlint rules.')
@@ -18,4 +19,28 @@ async function main() {
   // If paths has not been specified, lint all files
   const files = getFilesToLint((summaryByRule && ALL_CONTENT_DIR) || paths || getChangedFiles())
   console.log(files)
+}
+
+function getFilesToLint(paths) {
+  const fileList = {
+    length: 0,
+    content: [],
+    data: [],
+  }
+
+  for (const path of paths) {
+    const extension = extname(path)
+    const isMdFile = extension === '.md'
+    const isDirectory = extension === ''
+    if (!isMdFile && !isDirectory) continue
+
+    if (isMdFile) {
+      // Add each file to the relevant fileList group
+      path.startsWith('data') ? fileList.data.push(path) : fileList.content.push(path)
+    }
+  }
+  // Add a total fileList length property
+  fileList.length = fileList.content.length + fileList.data.length
+
+  return fileList
 }
